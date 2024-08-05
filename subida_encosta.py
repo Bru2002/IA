2fde87b459372cb6d92c8a033d1349885edb2d75
Bruna <santos.schbruna@gmail.com>
2024-08-04 21:42:35 -0300

    comentarios-encosta

--- subida_encosta.py
+++ subida_encosta.py
@@ -15,24 +15,27 @@ def subida_encosta_reinicio_aleatorio():
         
         for i in range(8):
             for j in range(8):
-                if tabuleiro[i] != j:
-                    novo_tabuleiro = tabuleiro[:]
-                    novo_tabuleiro[i] = j
+                if tabuleiro[i] != j:#POS J =! Pos I
+                    novo_tabuleiro = tabuleiro[:] #copia
+                    novo_tabuleiro[i] = j #assume valor de j a pos i 
                     conflitos_novo = conflito.conflito(novo_tabuleiro)
                     
-                    if conflitos_novo < melhor_conflito:
+                    if conflitos_novo < melhor_conflito: #se conflito for menor que anterior segue
                         melhor_tabuleiro = novo_tabuleiro
                         melhor_conflito = conflitos_novo
         
-        if melhor_tabuleiro:
-            tabuleiro = melhor_tabuleiro
-            conflitos_atual = melhor_conflito
+        if melhor_tabuleiro: #existe um tab melhor?
+            tabuleiro = melhor_tabuleiro #assume valor do tabuleiro melhor ao tabuleiro atual
+            conflitos_atual = melhor_conflito #assume valor do conflito menor ao numero atual de conflito
             # Registra a qualidade da solução
-            evolucao.append((reinicios, conflitos_atual))
+            evolucao.append((reinicios, conflitos_atual)) # cria uma lista de evol. para apresentar a analise e testes volta ao for
         else:
-            # Reinício aleatório
-            tabuleiro = gerar_aleatorio.gerar_aleatorio()
+            # Reinício aleatório se não existir melhor
+            tabuleiro = gerar_aleatorio.gerar_aleatorio() 
             conflitos_atual = conflito.conflito(tabuleiro)
-            reinicios += 1
+            reinicios += 1 # cont reinicia +1 volta ao for
     
     return tabuleiro, evolucao, reinicios
+#O teste de mesa deve ser repetido para cada combinação de i e j,
+#com a atualização do melhor_tabuleiro e melhor_conflito conforme necessário. 
+#Este teste ajuda a verificar a lógica de encontrar o tabuleiro com menos conflitos.
